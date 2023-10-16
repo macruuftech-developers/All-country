e890905aca9766be423d6d0adc4479c466aa866d
abdirahmanmahamoud <abdirahmanmahamoud14@gmail.com>
2023-10-16 22:19:36 +0300

    search

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const url = "https://restcountries.com/v3.1/all";
 let row = document.querySelector(".row-co");
+let search = document.querySelector("#search");
+let refresh = document.querySelector("#refresh");
 
 const api = async () => {
   let api = await fetch(url);
@@ -10,7 +12,7 @@ const api = async () => {
   data.map((element) => {
     country += ` <div class="box">
     <img src="${element.flags.png}" />
-    <h1>${element.name.common}</h1>
+    <h1 id='name'>${element.name.common}</h1>
     <div class="list">
       <div class="row">
         <span>Caasamada:</span>
@@ -36,3 +38,19 @@ const api = async () => {
 };
 
 api();
+
+search.addEventListener("keyup", () => {
+  let value = search.value;
+  document.querySelectorAll("#name").forEach((items) => {
+    if (items.firstChild.textContent.toLowerCase().indexOf(value) !== -1) {
+      items.parentElement.style.display = "block";
+    } else {
+      items.parentElement.style.display = "none";
+    }
+  });
+});
+
+refresh.addEventListener("click", () => {
+  search.value = "";
+  api();
+});
